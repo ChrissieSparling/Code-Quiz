9190d4d3cf969180e4b981c2f48ac46ce8edd0f6
Chrissie Sparling <gigglewrks@aol.com>
2021-09-28 16:09:45 -0700

    js updated

---
+++ script.js
@@ -0,0 +1,22 @@
+quizQuestions = [
+     {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+     },
+     {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+     }
+    ]
+    //add global variables for html and timer state
+
+    //start quiz function that hides start screen starts timer and unhides questions
+
+    //function to cycle questions
+
+    //function to check right or wrong
+
+    //function to end quiz hide question screen, unhide end sceen, and stop timer
+    
