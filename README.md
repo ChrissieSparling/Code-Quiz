b29ab974cb34f3807c6145fc230afc62d9e75205
Chrissie Sparling <gigglewrks@aol.com>
2021-09-28 17:47:59 -0700

    final project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Code-Quiz
 a timed quiz
+html-
+created the content
+
+css-
+gave it a small box
+
+js-
+added javascript to shift through the multiple choice questions and provide answers.
+also- negates time when you answer wrong.
+
--- script.js
+++ script.js
@@ -23,6 +23,7 @@ var answerchoices = document.getElementById("choices");
 var startbutton = document.getElementById("startBtn");
 var startScreen = document.getElementById ("start-screen")
 var questionTitle = document.getElementById ("question-title")
+var endQuiz = document.getElementById ("end-screen")
 
 function add (num1, num2){
 var total = num1 + num2
@@ -32,7 +33,7 @@ add(3, 4)
 
  //start quiz function that hides start screen starts timer and unhides questions
  function startQuiz(){
-     startScreen.setAttribute("class","hide");
+     startScreen.setAttribute("class","hide");//end portion needs to say questions.setattribute("class", "hide")
      questions.removeAttribute("class");
      timerState=setInterval(function(){
           time=time-1;
@@ -52,7 +53,7 @@ cycleQuestions()
 function cycleQuestions(){
      var displayQuestion = quizQuestions[questionIndex]
      questionTitle.textContent=displayQuestion.title
-
+answerchoices.innerHTML=""
      displayQuestion.choices.forEach((choice, i) => {
           // create new button for each choice
           var answerbutton = document.createElement("button");
@@ -60,16 +61,39 @@ function cycleQuestions(){
           answerbutton.setAttribute("value", choice);
           answerbutton.textContent = choice;
      //check answer function here
-
+answerbutton.onclick=checkAnswer
 
           // display on the page
           answerchoices.appendChild(answerbutton);
         });
 }
     //function to check right or wrong
+function checkAnswer(){
+     if (this.value===quizQuestions[questionIndex].answer
+     ){
+          console.log("correct")
+          //add right or wrong feedback here
+          
 
+     } else{
+          console.log("You are Sooo Wrong")
+     //add feeback 
+     time=time-10
+     timerDisplay.textContent=time
+     }
+
+questionIndex=questionIndex+1
+if(questionIndex===quizQuestions.length){
+     //quiz end function here
+     console.log ("Your done!")
+} else{
+     cycleQuestions()
+}
+
+    
+}
     //function to end quiz hide question screen, unhide end sceen, and 
     //stop timer.clearInterval(timerState)
-
+    
 //start button
 startbutton.onclick=startQuiz
